342a1f6d9f7f58e0782e5a98e79d9a104cdfddaa
riddhi-tamrakar <riddhitamrakar345@gmail.comm>
2021-06-09 02:16:32 -0700

    implement task

--- lib/src/modules/home/models/imageData.dart
+++ lib/src/modules/home/models/imageData.dart
@@ -1,38 +1,5 @@
 import 'package:equatable/equatable.dart';
 
-// class ImageData extends Equatable {
-//   final String coverUrl;
-
-//   const ImageData({
-//     this.coverUrl,
-//   });
-
-//   factory ImageData.fromJson(Map<String, dynamic> json) {
-//     return ImageData(
-//       coverUrl: json['cover_url'] as String,
-//     );
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'cover_url': coverUrl,
-//     };
-//   }
-
-//   ImageData copyWith({
-//     String coverUrl,
-//   }) {
-//     return ImageData(
-//       coverUrl: coverUrl ?? this.coverUrl,
-//     );
-//   }
-
-//   @override
-//   bool get stringify => true;
-
-//   @override
-//   List<Object> get props => [coverUrl];
-// }
 class ImageData {
   final String coverUrl;
 
